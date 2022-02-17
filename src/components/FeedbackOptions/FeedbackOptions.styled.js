19953d9120efb6f504126d5763b2060b547c85ae
Saharok0707 <saharok0707@gmail.com>
2022-02-17 23:40:27 +0200

    hhh

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const BtnList = styled.div`
+display: flex;
+justify-content: space-between;
+width: 300px;
+
+`;
+
+export const Button = styled.button`
+border: none;
+width: 50px;
+box-shadow: 0px 0px 12px -2px rgba(0,0,0,0.5);
+line-height: 1.25;
+  border-radius: 5px;
+ cursor: pointer;
+  position: relative;
+background-color: #ccc;
+`;
